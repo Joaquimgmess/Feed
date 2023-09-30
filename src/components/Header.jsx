f0eeca0d7f82a6e1e644e90355dc419dfcd5283e
Joaquim Gomes <joaquimgmess21@gmail.com>
2023-09-30 12:44:26 -0300

    iniciando projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header(){
+  return(
+    <div className="bg-slate-800 w-full h-20 flex justify-center items-center">
+      <h1 >Teste</h1>
+    </div>
+)
+}
+
+export default Header
